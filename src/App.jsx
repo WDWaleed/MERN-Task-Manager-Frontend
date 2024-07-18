751face31107e3e9dc2c2438e4bc27e5bcb81bc2
Waleed Ahmed <0100waleedahmed0100@gmail.com>
2024-07-18 09:26:22 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <main className="h-screen w-full bg-slate-700 text-white font-sans flex justify-center items-center ">
+        <h1 className="text-4xl">WDWaleed</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
