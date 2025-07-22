22dbf87179a7cc59c785df397a41698758a3a2d3
Waleed Ahmed <0100waleedahmed0100@gmail.com>
2025-07-22 12:58:50 +0500

    Fixing vercel routing issues
    I'm using react router, so when the user is navigated to a page via Link or
    navigate, it was working properly. But as soon as I refresh the site while
    being on one of these routes, Vercel was looking for a file by that route's name
    which of course doesn't exist.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true,
+  },
   // base: "/MERN-Task-Manager/",
   // server: {
   //   port: 3001,
